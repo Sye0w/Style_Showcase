5a8b2b646c0162e4c44bede6bf2ee9cad27a8328
Silas Owu <owusilasyp00@gmail.com>
2024-09-11 10:00:00 -0700

    💄 Style: add header ui

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ThemeSwitchComponent } from "../theme-switch/theme-switch.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ThemeSwitchComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
---
+++ src/app/components/theme-switch/theme-switch.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-switch',
+  standalone: true,
+  imports: [],
+  templateUrl: './theme-switch.component.html',
+  styleUrl: './theme-switch.component.scss'
+})
+
+export class ThemeSwitchComponent {
+  themeMode: boolean = false;
+}
