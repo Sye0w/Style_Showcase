f6641853f23e2dee27372628897116e7b89d7fff
Silas Owu <owusilasyp00@gmail.com>
2024-09-12 06:36:56 -0700

    💄 Style: add ui for main-content 2 with assets

--- src/app/components/main-content-2/main-content-2.component.ts
+++ src/app/components/main-content-2/main-content-2.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../model/services/data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-content-2',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-content-2.component.html',
   styleUrl: './main-content-2.component.scss'
 })
 
 export class MainContent2Component {
-  remainingItems = this.dataService.getRemainingItems()
+  remainingItems$ = this.dataService.getRemainingItems()
 
   constructor(private dataService: DataService ){}
 
